33f9c7611a8008edab5024a734271e2eccd5aa9d
Ditte Marie Nordlund Boye <s246215@dtu.dk>
2026-01-14 16:22:55 +0100

    Fix pytest collection and error type

---
+++ tests/test_model.py
@@ -0,0 +1,20 @@
+# tests/test_model.py
+import torch
+import pytest
+
+from mnist.model import MyAwesomeModel
+
+
+def test_model_output_shape():
+    model = MyAwesomeModel()
+    x = torch.randn(1, 1, 28, 28)
+    y = model(x)
+
+    assert y.shape == (1, 10), "Model output shape incorrect"
+
+
+def test_error_on_wrong_shape():
+    model = MyAwesomeModel()
+
+    with pytest.raises(RuntimeError):
+        model(torch.randn(1, 2, 28, 28))
